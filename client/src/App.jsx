bbdaa53c3a8ce06f1b60650fdaf1cad0e90c6c60
alishabohara03 <alishabohara369@gmail.com>
2025-05-02 15:51:14 +0545

    Fix case-sensitive import for About.jsx and add missing file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,102 +1,6 @@
 
 
 
-// import { Routes, Route } from "react-router-dom";
-// import AuthLayout from "./components/auth/layout";
-// import AuthLogin from "./pages/auth/login";
-// import AuthRegister from "./pages/auth/register";
-// import AdminLayout from "./components/admin-view/layout";
-// import AdminDashboard from "./pages/admin-view/dashboard";
-// import AdminProducts from "./pages/admin-view/products";
-// import AdminOrders from "./pages/admin-view/orders";
-// import AdminFeatures from "./pages/admin-view/features";
-// import ShoppingLayout from "./components/shopping-view/layout";
-// import NotFound from "./pages/not-found";
-// import ShoppingHome from "./pages/shopping-view/home";
-// import ShoppingListing from "./pages/shopping-view/listing";
-// import ShoppingCheckout from "./pages/shopping-view/checkout";
-// import ShoppingAccount from "./pages/shopping-view/account";
-// import CheckAuth from "./components/common/check-auth";
-// import UnauthPage from "./pages/unauth-page";
-// import SearchProducts from "./pages/shopping-view/search";
-
-
-
-// function App() {
-//   const isAuthenticated = true;
-//   const user = {
-//     name: "Alisha",
-//     role: "admin", // Add role to avoid undefined behavior
-//   };
-
-//   return (
-//     <div className="flex flex-col overflow-hidden bg-white">
-//       <Routes>
-//         {/*  Allow Unauthenticated Users to Access Auth Pages */}
-
-
-
-
-//         <Route path="/auth/*" element={<AuthLayout />}>
-//           <Route path="login" element={<AuthLogin />} />
-//           <Route path="register" element={<AuthRegister />} />
-//         </Route>
-
-//         {/* Protect Admin Routes */}
-//         <Route
-//           path="/admin/*"
-//           element={
-//             <CheckAuth isAuthenticated={isAuthenticated} user={user}>
-//               <AdminLayout />
-//             </CheckAuth>
-//           }
-//         >
-//           <Route path="dashboard" element={<AdminDashboard />} />
-//           <Route path="products" element={<AdminProducts />} />
-//           <Route path="orders" element={<AdminOrders />} />
-//           <Route path="features" element={<AdminFeatures />} />
-//         </Route>
-
-//         {/*  Protect Shopping Routes */}
-//         <Route
-//           path="/shop/*"
-//           element={
-//             <CheckAuth isAuthenticated={isAuthenticated} user={user}>
-//               <ShoppingLayout />
-//             </CheckAuth>
-//           }
-//         >
-//           <Route path="home" element={<ShoppingHome />} />
-//           <Route path="listing" element={<ShoppingListing />} />
-//           <Route path="checkout" element={<ShoppingCheckout />} />
-//           <Route path="account" element={<ShoppingAccount />} />
-
-//           <Route path="search" element={<SearchProducts />} />
-
-         
-
-
-
-        
-
-
-
-//         </Route>
-
-//         {/*  Unauthorized Page */}
-//         <Route path="/unauth-page" element={<UnauthPage />} />
-
-//         {/*  Catch-All Not Found */}
-//         <Route path="*" element={<NotFound />} />
-//       </Routes>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
 
 
 
@@ -129,7 +33,8 @@ import ShoppingAccount from "./pages/shopping-view/account";
 import CheckAuth from "./components/common/check-auth";
 import UnauthPage from "./pages/unauth-page";
 import SearchProducts from "./pages/shopping-view/search";
-import About from "./pages/shopping-view/about";
+// import About from "./pages/shopping-view/about";
+import About from "./pages/shopping-view/About";
 import Contact from "./pages/shopping-view/contact";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useDispatch, useSelector } from "react-redux";
